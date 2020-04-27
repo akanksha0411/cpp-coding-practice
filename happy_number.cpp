f283e72d8c96975e0de41479ebdd2edf0f1a8948
Akanksha <akankshasukhramani@gmail.com>
2020-04-27 06:23:22 +0530

    added happy_number code

---
+++ happy_number.cpp
@@ -0,0 +1,47 @@
+// A happy number is a number defined by the following process: Starting with any positive integer,
+// replace the number by the sum of the squares of its digits, and repeat the process until the
+// number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. 
+// Those numbers for which this process ends in 1 are happy numbers.
+
+#include <iostream>
+#include <string>
+#include <set>
+
+ using namespace std;
+
+ class Solution{
+     public:
+        int number_squared(int n){
+            int square = 0;
+            while(n){
+                square += (n%10) * (n%10);
+                n = n/10;
+            }
+            return square;
+        }
+
+        bool isHappy(int n){
+            set <int> s;
+            while(1){
+                n = number_squared(n);
+                if(n == 1){
+                    return true;
+                }
+                if(s.find(n) != s.end()){
+                    return false;
+                }
+                s.insert(n);
+            }
+        }
+ };
+
+ int main(){
+     int n = 19;
+     Solution x = Solution();
+      cout << x.isHappy(n) << "\n";
+     return 0;
+ }
+
+ // 19 is my number
+ // 19%10 = 9 (one digit)
+ // 9%10 = 
