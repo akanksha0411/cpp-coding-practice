1925db5b121f503b583b9a10c6fa543143fb84a5
Akanksha <akankshasukhramani@gmail.com>
2020-04-28 22:05:49 +0530

    added maxSubArrayCode

---
+++ maximum_subarray.cpp
@@ -0,0 +1,32 @@
+// Given an integer array nums, find the contiguous subarray (containing at least one number) 
+// which has the largest sum and return its sum.
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <set>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int max_sum = nums[0];
+        int individual_sum = nums[0]; 
+        for(int i=1; i<nums.size(); i++){
+            individual_sum = max(nums[i], individual_sum+nums[i]);
+            if(individual_sum>max_sum){
+                max_sum = individual_sum;
+            }
+        }
+    return max_sum;
+    }
+};
+
+int main(){
+    vector<int> v = {2, 3, -1, -4, 4, 8, 19};
+
+    Solution x = Solution();
+    cout << x.maxSubArray(v);
+}
